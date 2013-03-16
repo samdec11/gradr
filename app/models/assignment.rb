34701711928d9d344f0fbb7643d48e62e066e86f
NickyH <nickyhughes00@gmail.com>
2013-03-16 17:23:31 -0400

    Seeded database

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,33 +4,13 @@
 #
 #  id         :integer          not null, primary key
 #  name       :string(255)
-#  groupsize  :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class Assignment < ActiveRecord::Base
-  attr_accessible :name, :groupsize
-  validates :name, :groupsize, :presence => true
-  validates :groupsize, :numericality =>[:greater_than => 0]
+  attr_accessible :name
+  validates :name, :presence => true
   has_one :grade
   belongs_to :group
-
-  before_save :make_groups
-  private
-  def make_groups
-    if Student.all != []
-      group_count = Student.count / self.groupsize
-      i = 1
-      shuffled_class = Student.all.shuffle
-      group_count.times do
-        pop = shuffled_class.pop(self.groupsize)
-        group = Group.create(:name =>"Group #{i}")
-        group.students << pop
-        self.groups << group
-        i = i + 1
-      end
-      self.groups.last.students << shuffled_class
-    end
-  end
 end
--- db/migrate/20130314215027_add_assignments_table.rb
+++ db/migrate/20130314215027_add_assignments_table.rb
@@ -2,7 +2,6 @@ class AddAssignmentsTable < ActiveRecord::Migration
   def change
     create_table :assignments do |t|
       t.string :name
-      t.integer :groupsize
       t.timestamps
     end
   end

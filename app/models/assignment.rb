ab244cba47846151f805b63a00b520c3a647b7d0
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-16 17:24:17 -0400

    merged

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -15,22 +15,4 @@ class Assignment < ActiveRecord::Base
   validates :groupsize, :numericality =>[:greater_than => 0]
   has_one :grade
   belongs_to :group
-
-  before_save :make_groups
-  private
-  def make_groups
-    if Student.all != []
-      group_count = Student.count / self.groupsize
-      i = 1
-      shuffled_class = Student.all.shuffle
-      group_count.times do
-        pop = shuffled_class.pop(self.groupsize)
-        group = Group.create(:name =>"Group #{i}")
-        group.students << pop
-        self.groups << group
-        i = i + 1
-      end
-      self.groups.last.students << shuffled_class
-    end
-  end
 end
